c34ae041f7f221e07ec83a6c425af972cefe1b6b
Michael <michaelgiu2012@outlook.com>
2024-12-09 18:28:04 -0500

    Reformat

--- src/main/java/net/michael/openplease/OpenPlease.java
+++ src/main/java/net/michael/openplease/OpenPlease.java
@@ -73,86 +73,86 @@ public class OpenPlease {
     }
 
 
-        public static final float DOOR_DISTANCE = 1;
+    public static final float DOOR_DISTANCE = 1;
 
-        public OpenPlease() {
-            // Register event handlers
-            MinecraftForge.EVENT_BUS.register(this);
-        }
+    public OpenPlease() {
+        // Register event handlers
+        MinecraftForge.EVENT_BUS.register(this);
+    }
 
-        @SubscribeEvent
-        public void onServerTick(TickEvent.LevelTickEvent event) {
-            // Ensure we are on the server side and in the END phase of the tick
-            if (event.level instanceof ServerLevel world && event.phase == TickEvent.Phase.END) {
-                for (Player player : world.players()) {
-                    BlockPos playerPos = player.blockPosition();
+    @SubscribeEvent
+    public void onServerTick(TickEvent.LevelTickEvent event) {
+        // Ensure we are on the server side and in the END phase of the tick
+        if (event.level instanceof ServerLevel world && event.phase == TickEvent.Phase.END) {
+            for (Player player : world.players()) {
+                BlockPos playerPos = player.blockPosition();
 
-                    // Check surrounding blocks within 4 blocks
-                    for (int x = -4; x <= 4; x++) {
-                        for (int y = -4; y <= 4; y++) {
-                            for (int z = -4; z <= 4; z++) {
-                                BlockPos pos = playerPos.offset(x, y, z);
-                                if (isDoor(world, pos)) {
-                                    handleDoor(world, pos, playerPos);
-                                }
-                                if (isTrapdoor(world, pos)) {
-                                    handleTrapdoor(world, pos, playerPos);
-                                }
-                                if (isFenceGate(world, pos)) {
-                                    handleFenceGate(world, pos, playerPos);
-                                }
+                // Check surrounding blocks within 4 blocks
+                for (int x = -4; x <= 4; x++) {
+                    for (int y = -4; y <= 4; y++) {
+                        for (int z = -4; z <= 4; z++) {
+                            BlockPos pos = playerPos.offset(x, y, z);
+                            if (isDoor(world, pos)) {
+                                handleDoor(world, pos, playerPos);
+                            }
+                            if (isTrapdoor(world, pos)) {
+                                handleTrapdoor(world, pos, playerPos);
+                            }
+                            if (isFenceGate(world, pos)) {
+                                handleFenceGate(world, pos, playerPos);
                             }
                         }
                     }
                 }
             }
         }
+    }
 
-        private boolean isDoor(Level world, BlockPos pos) {
-            Block block = world.getBlockState(pos).getBlock();
-            return block instanceof DoorBlock;
+    private boolean isDoor(Level world, BlockPos pos) {
+        Block block = world.getBlockState(pos).getBlock();
+        return block instanceof DoorBlock;
+    }
+
+    private boolean isTrapdoor(Level world, BlockPos pos) {
+        Block block = world.getBlockState(pos).getBlock();
+        return block instanceof TrapDoorBlock;
+    }
+
+    private boolean isFenceGate(Level world, BlockPos pos) {
+        Block block = world.getBlockState(pos).getBlock();
+        return block instanceof FenceGateBlock;
+    }
+
+    private void handleDoor(Level world, BlockPos doorPos, BlockPos playerPos) {
+        double distance = playerPos.distSqr(new Vec3i(doorPos.getX(), doorPos.getY(), doorPos.getZ()));
+
+        boolean isOpen = world.getBlockState(doorPos).getValue(DoorBlock.OPEN);
+        if (distance <= ((DOOR_DISTANCE + 1) * (DOOR_DISTANCE + 1)) && !isOpen) {
+            world.setBlock(doorPos, world.getBlockState(doorPos).setValue(DoorBlock.OPEN, true), 3);
+        } else if (distance > ((DOOR_DISTANCE + 1) * (DOOR_DISTANCE + 1)) && isOpen) {
+            world.setBlock(doorPos, world.getBlockState(doorPos).setValue(DoorBlock.OPEN, false), 3);
         }
+    }
 
-        private boolean isTrapdoor(Level world, BlockPos pos) {
-            Block block = world.getBlockState(pos).getBlock();
-            return block instanceof TrapDoorBlock;
+    private void handleTrapdoor(Level world, BlockPos trapdoorPos, BlockPos playerPos) {
+        double distance = playerPos.distSqr(new Vec3i(trapdoorPos.getX(), trapdoorPos.getY(), trapdoorPos.getZ()));
+
+        boolean isOpen = world.getBlockState(trapdoorPos).getValue(TrapDoorBlock.OPEN);
+        if (distance <= DOOR_DISTANCE * DOOR_DISTANCE && !isOpen) {
+            world.setBlock(trapdoorPos, world.getBlockState(trapdoorPos).setValue(TrapDoorBlock.OPEN, true), 3);
+        } else if (distance > DOOR_DISTANCE * DOOR_DISTANCE && isOpen) {
+            world.setBlock(trapdoorPos, world.getBlockState(trapdoorPos).setValue(TrapDoorBlock.OPEN, false), 3);
         }
+    }
 
-        private boolean isFenceGate(Level world, BlockPos pos) {
-            Block block = world.getBlockState(pos).getBlock();
-            return block instanceof FenceGateBlock;
+    private void handleFenceGate(Level world, BlockPos fenceGatePos, BlockPos playerPos) {
+        double distance = playerPos.distSqr(new Vec3i(fenceGatePos.getX(), fenceGatePos.getY(), fenceGatePos.getZ()));
+
+        boolean isOpen = world.getBlockState(fenceGatePos).getValue(FenceGateBlock.OPEN);
+        if (distance <= DOOR_DISTANCE * DOOR_DISTANCE && !isOpen) {
+            world.setBlock(fenceGatePos, world.getBlockState(fenceGatePos).setValue(FenceGateBlock.OPEN, true), 3);
+        } else if (distance > DOOR_DISTANCE * DOOR_DISTANCE && isOpen) {
+            world.setBlock(fenceGatePos, world.getBlockState(fenceGatePos).setValue(FenceGateBlock.OPEN, false), 3);
         }
-
-        private void handleDoor(Level world, BlockPos doorPos, BlockPos playerPos) {
-            double distance = playerPos.distSqr(new Vec3i(doorPos.getX(), doorPos.getY(), doorPos.getZ()));
-
-            boolean isOpen = world.getBlockState(doorPos).getValue(DoorBlock.OPEN);
-            if (distance <= ((DOOR_DISTANCE + 1) * (DOOR_DISTANCE + 1)) && !isOpen) {
-                world.setBlock(doorPos, world.getBlockState(doorPos).setValue(DoorBlock.OPEN, true), 3);
-            } else if (distance > ((DOOR_DISTANCE + 1) * (DOOR_DISTANCE + 1)) && isOpen) {
-                world.setBlock(doorPos, world.getBlockState(doorPos).setValue(DoorBlock.OPEN, false), 3);
-            }
-        }
-
-        private void handleTrapdoor(Level world, BlockPos trapdoorPos, BlockPos playerPos) {
-            double distance = playerPos.distSqr(new Vec3i(trapdoorPos.getX(), trapdoorPos.getY(), trapdoorPos.getZ()));
-
-            boolean isOpen = world.getBlockState(trapdoorPos).getValue(TrapDoorBlock.OPEN);
-            if (distance <= DOOR_DISTANCE * DOOR_DISTANCE && !isOpen) {
-                world.setBlock(trapdoorPos, world.getBlockState(trapdoorPos).setValue(TrapDoorBlock.OPEN, true), 3);
-            } else if (distance > DOOR_DISTANCE * DOOR_DISTANCE && isOpen) {
-                world.setBlock(trapdoorPos, world.getBlockState(trapdoorPos).setValue(TrapDoorBlock.OPEN, false), 3);
-            }
-        }
-
-        private void handleFenceGate(Level world, BlockPos fenceGatePos, BlockPos playerPos) {
-            double distance = playerPos.distSqr(new Vec3i(fenceGatePos.getX(), fenceGatePos.getY(), fenceGatePos.getZ()));
-
-            boolean isOpen = world.getBlockState(fenceGatePos).getValue(FenceGateBlock.OPEN);
-            if (distance <= DOOR_DISTANCE * DOOR_DISTANCE && !isOpen) {
-                world.setBlock(fenceGatePos, world.getBlockState(fenceGatePos).setValue(FenceGateBlock.OPEN, true), 3);
-            } else if (distance > DOOR_DISTANCE * DOOR_DISTANCE && isOpen) {
-                world.setBlock(fenceGatePos, world.getBlockState(fenceGatePos).setValue(FenceGateBlock.OPEN, false), 3);
-            }
-        }
+    }
 }
