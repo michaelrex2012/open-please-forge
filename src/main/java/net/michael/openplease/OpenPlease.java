74155d6c147bc38321034522d526fc44b1d41dca
Michael <michaelgiu2012@outlook.com>
2024-12-16 20:13:03 -0500

    Ported 1.1.3 to Forge

--- src/main/java/net/michael/openplease/OpenPlease.java
+++ src/main/java/net/michael/openplease/OpenPlease.java
@@ -2,17 +2,17 @@ package net.michael.openplease;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.ChatFormatting;
+import net.minecraft.SystemReport;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.DoorBlock;
-import net.minecraft.world.level.block.FenceGateBlock;
-import net.minecraft.world.level.block.TrapDoorBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -27,7 +27,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
-
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(OpenPlease.MOD_ID)
 public class OpenPlease {
@@ -111,12 +110,35 @@ public class OpenPlease {
                             BlockPos pos = playerPos.offset(x, y, z);
                             Block block = world.getBlockState(pos).getBlock();
 
-                            if (block instanceof DoorBlock) {
+                            if (block instanceof DoorBlock && block != Blocks.IRON_DOOR) {
+                                boolean oldStateDoor = world.getBlockState(pos).getValue(DoorBlock.OPEN);
                                 handleDoor(world, pos, playerPos);
-                            } else if (block instanceof TrapDoorBlock) {
+                                if (oldStateDoor && !world.getBlockState(pos).getValue(DoorBlock.OPEN)) {
+                                    world.playSound(null, pos, SoundEvents.WOODEN_DOOR_CLOSE, SoundSource.BLOCKS, 1.0f, 1.0f);
+                                }
+                                if (!oldStateDoor && world.getBlockState(pos).getValue(DoorBlock.OPEN)) {
+                                    world.playSound(null, pos, SoundEvents.WOODEN_DOOR_OPEN, SoundSource.BLOCKS, 1.0f, 1.0f);
+                                }
+                            }
+                            if (block instanceof TrapDoorBlock && block != Blocks.IRON_TRAPDOOR) {
+                                boolean oldStateTrapDoor = world.getBlockState(pos).getValue(TrapDoorBlock.OPEN);
                                 handleTrapdoor(world, pos, playerPos);
-                            } else if (block instanceof FenceGateBlock) {
+                                if (oldStateTrapDoor && !world.getBlockState(pos).getValue(TrapDoorBlock.OPEN)) {
+                                    world.playSound(null, pos, SoundEvents.WOODEN_TRAPDOOR_CLOSE, SoundSource.BLOCKS, 1.0f, 1.0f);
+                                }
+                                if (!oldStateTrapDoor && world.getBlockState(pos).getValue(TrapDoorBlock.OPEN)) {
+                                    world.playSound(null, pos, SoundEvents.WOODEN_TRAPDOOR_OPEN, SoundSource.BLOCKS, 1.0f, 1.0f);
+                                }
+                            }
+                            if (block instanceof FenceGateBlock) {
+                                boolean oldStateFenceGate = world.getBlockState(pos).getValue(FenceGateBlock.OPEN);
                                 handleFenceGate(world, pos, playerPos);
+                                if (oldStateFenceGate && !world.getBlockState(pos).getValue(FenceGateBlock.OPEN)) {
+                                    world.playSound(null, pos, SoundEvents.FENCE_GATE_CLOSE, SoundSource.BLOCKS, 1.0f, 1.0f);
+                                }
+                                if (!oldStateFenceGate && world.getBlockState(pos).getValue(FenceGateBlock.OPEN)) {
+                                    world.playSound(null, pos, SoundEvents.FENCE_GATE_OPEN, SoundSource.BLOCKS, 1.0f, 1.0f);
+                                }
                             }
                         }
                     }
@@ -128,7 +150,7 @@ public class OpenPlease {
     private void handleDoor(Level world, BlockPos doorPos, BlockPos playerPos) {
         double distance = playerPos.distSqr(doorPos);
         boolean isOpen = world.getBlockState(doorPos).getValue(DoorBlock.OPEN);
-        boolean shouldBeOpen = distance <= DOOR_DISTANCE * DOOR_DISTANCE;
+        boolean shouldBeOpen = distance <= DOOR_DISTANCE + 2 * DOOR_DISTANCE + 2;
 
         if (isOpen != shouldBeOpen) {
             world.setBlock(doorPos, world.getBlockState(doorPos).setValue(DoorBlock.OPEN, shouldBeOpen), 3);
